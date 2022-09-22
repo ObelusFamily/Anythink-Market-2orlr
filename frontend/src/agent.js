cb4c4cd97db84acc0042abbcc590b93d90559b46
rida <rida_538@hotmail.com>
2022-09-22 16:29:10 +0000

    .

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -4,7 +4,9 @@ import _superagent from "superagent";
 const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
-  process.env.NODE_ENV !== "production" ? "http://localhost:3000": "https://api.anythink.market";
+  process.env.NODE_ENV !== "production"
+    ? process.env.REACT_APP_BACKEND_URL
+    : "https://api.anythink.market";
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
